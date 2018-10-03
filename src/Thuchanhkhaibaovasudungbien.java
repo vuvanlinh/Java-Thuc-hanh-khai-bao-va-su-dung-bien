a6a35af21a1616076ec7415fdeaf143eefa68e61
vuvanlinh <vuvanlinh95@gmail.com>
2018-10-03 19:09:35 +0700

    thuc hanh khai bao va su dung bien

---
+++ src/Thuchanhkhaibaovasudungbien.java
@@ -0,0 +1,18 @@
+public class Thuchanhkhaibaovasudungbien {
+    public static void main(String[] args) {
+        int i = 10;
+        double f = 20.5;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha Noi";
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+
+    }
+}
